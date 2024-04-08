6d6ae77045d88312e268a14dd86409f2afe78854
zerreat <arpan.11.1991@gmail.com>
2024-04-09 01:06:51 +0530

    First pass at 2 hrs

---
+++ frontend/src/components/MobileNavLinks.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+const MobileNavLinks = () => {
+  return(
+    <>
+        <Link to ="/user-profile" className="flex bg-white items-center font-bold hover:text-orange-500">
+            User Profile
+        </Link>
+        <Button className="flex items-center  px-3 font-bold hover:bg-gray-500">
+            Log Out
+        </Button>
+    </>
+  )
+}
+
+export default MobileNavLinks;
